266df3a483d5c01c9d5ca08ec4ebf5f0be9e37b0
Chris Simpkins <git.simpkins@gmail.com>
2019-06-02 15:31:21 -0400

    [app.py] blacken source

--- fontware/app.py
+++ fontware/app.py
@@ -16,23 +16,23 @@ app.config.update(
     SECRET_KEY=SECRET_KEY,
     # Flask-Dropzone config:
     DROPZONE_ALLOWED_FILE_CUSTOM=True,
-    DROPZONE_ALLOWED_FILE_TYPE='.ttf, .otf',
+    DROPZONE_ALLOWED_FILE_TYPE=".ttf, .otf",
     DROPZONE_MAX_FILE_SIZE=25,
     DROPZONE_MAX_FILES=1,
-    DROPZONE_ENABLE_CSRF=True  # enable CSRF protection
+    DROPZONE_ENABLE_CSRF=True,  # enable CSRF protection
 )
 
 dropzone = Dropzone(app)
 csrf = CSRFProtect(app)  # initialize CSRFProtect
 
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route("/", methods=["POST", "GET"])
 def upload():
-    if request.method == 'POST':
-        f = request.files.get('file')
+    if request.method == "POST":
+        f = request.files.get("file")
         tt = TTFont(f)
         # f.save(os.path.join(app.config['UPLOADED_PATH'], f.filename))
-    return render_template('index.html')
+    return render_template("index.html")
 
 
 # handle CSRF error
@@ -41,5 +41,5 @@ def csrf_error(e):
     return e.description, 400
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=False)
